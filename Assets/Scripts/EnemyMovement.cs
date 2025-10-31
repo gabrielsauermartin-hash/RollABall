e6651ba75bcf5149cdd41e6461455b927eabec8e
gabriel sauer <gabrielsauermartin@alumno.ieselrincon.es>
2025-10-31 12:26:49 +0100

    Creación de Patrullas y cono de visión de enemigos

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,6 +4,87 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     public Transform player;
+    public float visionRadius = 8f;
+    public float visionAngle = 45f;
+    public LayerMask obstacleMask;
+    public Transform[] patrolPoints;
+    private int currentPatrolIndex = 0;
+
+    private NavMeshAgent agent;
+    private bool playerInVision = false;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (patrolPoints.Length > 0)
+        {
+            agent.SetDestination(patrolPoints[currentPatrolIndex].position);
+        }
+    }
+
+    void Update()
+    {
+        Vector3 dirToPlayer = (player.position - transform.position).normalized;
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < visionRadius)
+        {
+            float angle = Vector3.Angle(transform.forward, dirToPlayer);
+            if (angle < visionAngle / 2f)
+            {
+                if (!Physics.Raycast(transform.position, dirToPlayer, distanceToPlayer, obstacleMask))
+                {
+                    playerInVision = true;
+                    agent.SetDestination(player.position);
+                }
+                else
+                {
+                    LostPlayer();
+                }
+            }
+            else
+            {
+                LostPlayer();
+            }
+        }
+        else
+        {
+            LostPlayer();
+        }
+    }
+
+    private void LostPlayer()
+    {
+        if (playerInVision)
+        {
+            playerInVision = false;
+            if(patrolPoints.Length > 0)
+                agent.SetDestination(patrolPoints[currentPatrolIndex].position);
+        }
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+            agent.SetDestination(patrolPoints[currentPatrolIndex].position);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Dibuja el radio de visión
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, visionRadius);
+
+        // Dibuja el ángulo de visión
+        Vector3 leftBoundary = Quaternion.Euler(0, -visionAngle / 2f, 0) * transform.forward * visionRadius;
+        Vector3 rightBoundary = Quaternion.Euler(0, visionAngle / 2f, 0) * transform.forward * visionRadius;
+        Gizmos.DrawLine(transform.position, transform.position + leftBoundary);
+        Gizmos.DrawLine(transform.position, transform.position + rightBoundary);
+    }
+
+    
+    /*
+    public Transform player;
     private NavMeshAgent navMeshAgent; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,4 +100,5 @@ public class EnemyMovement : MonoBehaviour
             navMeshAgent.SetDestination(player.position);
         }
     }
+    */
 }
---
+++ README.md
@@ -0,0 +1,38 @@
+# Roll-a-ball
+
+Un proyecto hecho en Unity como práctica para realizar videojuegos sencillos en 3D. Este proyecto 
+contiene mecánicas de salto, recolección de objetos, IA de enemigos con patrullas y conos de visión, 
+obstáculos, contador de puntos y la capacidad de reiniciar una partida una vez ganada o perdida.
+
+### Prerequisitos
+
+Es necesario tener una buena máquina para poder instalar y ejecutar proyectos de Unity, 
+cualquier máquina de baja gama tendrá problemas a la hora de operar en el proyecto.
+
+### Instalación
+
+1. Asegúrese de tener Unity instalado primero.
+
+2. Diríjase a el enlace de GitHub y descargue el proyecto:
+https://github.com/gabrielsauermartin-hash/RollABall
+
+3. Abra el proyecto dentro del entorno de Unity y ejecútelo (al ejecutarlo, deberían
+descargarse todas las librerías que fueron usadas en el proyecto).
+
+## Autor
+
+  - **Gabriel Sauer Martín**
+
+<!--
+## License
+
+This project is licensed under the [CC0 1.0 Universal](LICENSE.md)
+Creative Commons License - see the [LICENSE.md](LICENSE.md) file for
+details
+
+## Acknowledgments
+
+  - Hat tip to anyone whose code is used
+  - Inspiration
+  - etc
+->
